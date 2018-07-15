f75fdca57ecbfcd8c3c533c1f16fa5e4a9fe7a86
Alisdair <Owner@DESKTOP-I9G2QH6>
2018-07-15 16:58:21 +1200

    Changed required password

--- BookingSystem/Startup.cs
+++ BookingSystem/Startup.cs
@@ -39,6 +39,16 @@ namespace BookingSystem
             services.AddTransient<IEmailSender, EmailSender>();
 
             services.AddMvc();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 5;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
