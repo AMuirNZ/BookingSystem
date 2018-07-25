cac6444d85862050f5c80547282e2b370fba39dc
Alisdair <Owner@DESKTOP-I9G2QH6>
2018-07-25 15:41:25 +1200

    Removed unused section

--- BookingSystem/Startup.cs
+++ BookingSystem/Startup.cs
@@ -76,29 +76,10 @@ namespace BookingSystem
                     template: "{controller=Performances}/{action=Index}/{id?}");
             });
 
-            //CreateUserRoles(services).Wait();
+            
         }
 
-        private async Task CreateUserRoles(IServiceProvider serviceProvider)
-        {
-            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-            var UserManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
-
-
-            IdentityResult roleResult;
-            //Adding Addmin Role  
-            var roleCheck = await RoleManager.RoleExistsAsync("Admin");
-            if (!roleCheck)
-            {
-                //create the roles and seed them to the database  
-                roleResult = await RoleManager.CreateAsync(new IdentityRole("Admin"));
-            }
-            //Assign Admin role to the main User here we have given our newly loregistered login id for Admin management  
-            ApplicationUser user = await UserManager.FindByEmailAsync("admin@admin.com");
-            var User = new ApplicationUser();
-            await UserManager.AddToRoleAsync(user, "Admin");
-
-        }
+      
 
     }
 }
